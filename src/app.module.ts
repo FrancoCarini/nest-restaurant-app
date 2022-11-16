c9167ad7c012f6140224b6375304dbabf5bb1bed
Franco Carini <carini.franco@gmail.com>
2022-11-16 19:29:26 -0300

    Remove imports from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DishesModule } from './dishes/dishes.module';
 
 @Module({
   imports: [DishesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
